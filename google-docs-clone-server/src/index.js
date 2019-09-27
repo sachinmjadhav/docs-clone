e33720255b73eb95185a795f0d62e2c7a25dda07
sachinmjadhav <sachin0321@gmail.com>
2019-09-27 16:45:48 +0530

    initial commit

---
+++ google-docs-clone-server/src/index.js
@@ -0,0 +1,49 @@
+const app = require("express")();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+const cors = require("cors");
+
+let initilaEditorValue = {
+  document: {
+    nodes: [
+      {
+        object: "block",
+        type: "paragraph",
+        nodes: [
+          {
+            object: "text",
+            text: "A line of text in a paragraph."
+          }
+        ]
+      }
+    ]
+  }
+};
+
+const groupData = {};
+
+io.on("connection", function(socket) {
+  socket.on("new-operations", function(data) {
+    groupData[data.groupId] = data.value;
+    io.emit(`new-remote-operations-${data.groupId}`, data);
+  });
+});
+
+app.use(
+  cors({
+    origin: "http://localhost:3000"
+  })
+);
+
+app.get("/groups/:id", (req, res) => {
+  const { id } = req.params;
+  if (!(id in groupData)) {
+    groupData[id] = initilaEditorValue;
+  }
+
+  res.send(groupData[id]);
+});
+
+http.listen(4000, function() {
+  console.log("listening on *:4000");
+});
