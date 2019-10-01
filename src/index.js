facdce48f17fd42442317cf9089f760c441188af
sachinmjadhav <sachin0321@gmail.com>
2019-10-01 12:20:23 +0530

    changes port

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const app = require("express")();
+const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 const cors = require("cors");
+
 
 let initialEditorValue = {
   document: {
