9afbed0e2eaec2ee187cd4b6cc86aa81f33e737d
sachinmjadhav <sachin0321@gmail.com>
2019-10-03 14:01:51 +0530

    route re-fix

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,6 +6,7 @@ const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Route
+        exact
         path="/"
         render={() => {
           return <Redirect to={`/group/${Date.now()}`} />;
